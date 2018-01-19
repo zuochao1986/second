917909771de826bdaf7caec8e00b42b3e7668db9
ivan <81959090@qq.com>
2018-01-19 21:35:14 +0800

    second version

---
+++ src/main/java/com/ivan/servlet/HelloServlet1.java
@@ -0,0 +1,26 @@
+package com.ivan.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "HelloServlet1")
+public class HelloServlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
